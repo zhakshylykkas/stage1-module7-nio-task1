7c2ed5fd934bb3c1dfcb78fab910fb6204f698a7
Zhakshylyk Kasymbekov <jakkasymbekov@gmail.com>
2023-09-25 14:50:34 +0600

    Java NIO task commit

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,51 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+
+        try {
+            Path path = file.toPath();
+            List<String> lines = Files.readAllLines(path);
+
+            for (String line : lines) {
+                String[] arr = line.split(": ");
+
+                if (arr.length == 2) {
+                    String key = arr[0];
+                    String value = arr[1];
+
+                    switch (key) {
+                        case "Name":
+                            profile.setName(value);
+                            break;
+                        case "Age":
+                            profile.setAge(Integer.parseInt(value));
+                            break;
+                        case "Email":
+                            profile.setEmail(value);
+                            break;
+                        case "Phone":
+                            profile.setPhone(Long.parseLong(value));
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return profile;
     }
 }
